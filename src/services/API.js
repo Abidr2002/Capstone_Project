9e3372d9bb57825e789f18f019b77c31e60a8bc4
praset911 <praset911@gmail.com>
2023-12-16 11:53:40 +0700

    fix API & add vercel.json

--- src/services/API.js
+++ src/services/API.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
-const apiKey = '224fdd7405614aa3a515d6d61f3efa75';
-
-const getHealthNews = async () => {
+const getNewsData = async () => {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${apiKey}`
+      `https://api-berita-indonesia.vercel.app/suara/health/`
     );
-    return response.data.articles;
+    return response.data.data.posts || [];
   } catch (error) {
-    console.error('Error fetching health news:', error);
+    console.error('Error fetching news data:', error);
     return [];
   }
 };
 
-export default getHealthNews;
+export default getNewsData;
